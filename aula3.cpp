0b513d9e7b39b81d710fa3f94a34112275742efa
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2023-09-27 23:30:44 -0300

    Primeira aula, aula 3

---
+++ aula3.cpp
@@ -0,0 +1,15 @@
+// Estrutura básica de um programa em C++
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Canal Fessor Bruno \nCurso C++ \n"; // "\n" comando utilizado para quebra de linha
+    // cout << "Canal Fessor Bruno" << endl << "Curso C++" << endl;
+    // ...<< endl...; (end line) outro comando utilizado para quebra de linha
+
+    system("pause"); // comando utilizado para pausar o programa e aguardar a confirmação para finalizar
+    return 0;
+}
