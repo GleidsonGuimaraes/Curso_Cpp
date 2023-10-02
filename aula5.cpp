2132a3aa1924a107bc6acb541e53c4be0d8d329e
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2023-10-02 19:54:56 -0300

    aula 5

---
+++ aula5.cpp
@@ -0,0 +1,20 @@
+// Declaração múltipla de variáveis e Constantes
+
+#include <iostream>
+
+using namespace std;
+
+// declarando uma constante utilizando a diretiva #define
+// a diretiva #define permite adicionar uma função a constante
+#define pi 3.1415
+#define canal cout << "Canal Fessor Bruno.\n";
+
+int main(){
+
+    int vidas = 3, municao = 500, life = 100;
+
+    cout << vidas << "\n" << municao << "\n" << life << "\n" << pi << "\n";
+    canal
+
+    return 0;
+}
