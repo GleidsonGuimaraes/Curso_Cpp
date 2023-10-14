c971864812ae78bc0742ff3676e50ca2ecd4fe37
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2023-10-14 14:45:51 -0300

    aula 25

---
+++ aula25.cpp
@@ -0,0 +1,22 @@
+// Funções recursivas
+// Recursividade é quando uma função chama a si mesma n vezes
+
+#include <iostream>
+
+using namespace std;
+
+void contador(int num, int cont=0);
+
+int main(){
+
+    contador(20);
+
+    return 0;
+}
+
+void contador(int num, int cont){
+    cout << cont << "\n";
+    if(num > cont){
+        contador(num, ++cont);
+    }
+}
